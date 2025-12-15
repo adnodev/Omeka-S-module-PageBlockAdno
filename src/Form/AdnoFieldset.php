ccd5649771c0d7660edad9bd485aa8e14347959f
Jeau <thierrypasquier@gmail.com>
2025-12-15 08:09:19 +0100

    two settings require

--- src/Form/AdnoFieldset.php
+++ src/Form/AdnoFieldset.php
@@ -15,65 +15,67 @@ class AdnoFieldset extends Fieldset
                     'name' => 'o:block[__blockIndex__][o:data][instance]',
                     'type' => Element\Url::class,
                     'options' => [
-                    'label' => 'Adno instance URL', // @translate
-                    'info' => 'The address of your Adno instance. If you do not have one, you can use https://w.adno.app.', // @translate
+                        'label' => 'Adno instance URL', // @translate
+                        'info' => 'The address of your Adno instance. If you do not have one, you can use https://w.adno.app.', // @translate
                     ],
                     'attributes' => [
-                    'id' => 'adno-instance',
-                    'value' => 'https://w.adno.app',
-                    'pattern' => 'https?://.+'
+                        'id' => 'adno-instance',
+                        'required' => true,
+                        'value' => 'https://w.adno.app',
+                        'pattern' => 'https?://.+'
                     ],
             ])
             ->add([
                     'name' => 'o:block[__blockIndex__][o:data][src]',
                     'type' => Element\Url::class,
                     'options' => [
-                    'label' => 'Adno project URL', // @translate
-                    'info' => 'It is also possible to provide the URL of an image', // @translate
+                        'label' => 'Adno project URL', // @translate
+                        'info' => 'It is also possible to provide the URL of an image', // @translate
                     ],
                     'attributes' => [
-                    'id' => 'adno-src',
-                    'pattern' => 'https?://.+'
+                        'id' => 'adno-src',
+                        'required' => true,
+                        'pattern' => 'https?://.+'
                     ],
             ])
             ->add([
                     'name' => 'o:block[__blockIndex__][o:data][params]',
                     'type' => Element\Textarea::class,
                     'options' => [
-                    'label' => 'Parameters', // @translate
-                    'info' => "Use Adno's URL generator to override project settings. https://w.adno.app/#/link", // @translate
+                        'label' => 'Parameters', // @translate
+                        'info' => "Use Adno's URL generator to override project settings. https://w.adno.app/#/link", // @translate
                     ],
                     'attributes' => [
-                    'id' => 'adno-params',
-                    'rows' => 5,
+                        'id' => 'adno-params',
+                        'rows' => 5,
                     ],
             ])
             ->add([
                     'name' => 'o:block[__blockIndex__][o:data][width]',
                     'type' => Element\Text::class,
                     'options' => [
-                    'label' => 'Width', // @translate
-                    'info' => 'The width of the container in percentage (e.g., 100%) or in pixels (e.g., 600).', // @translate
+                        'label' => 'Width', // @translate
+                        'info' => 'The width of the container in percentage (e.g., 100%) or in pixels (e.g., 600).', // @translate
                     ],
                     'attributes' => [
-                    'id' => 'adno-width',
-                    'size' => 10,
-                    'maxlength' => 10,
-                    'pattern' => '[0-9]+%?'
+                        'id' => 'adno-width',
+                        'size' => 10,
+                        'maxlength' => 10,
+                        'pattern' => '[0-9]+%?'
                     ],
             ])
             ->add([
                     'name' => 'o:block[__blockIndex__][o:data][height]',
                     'type' => Element\Text::class,
                     'options' => [
-                    'label' => 'Height', // @translate
-                    'info' => 'The height of the container in percentage (e.g., 100%) or in pixels (e.g., 600).', // @translate
+                        'label' => 'Height', // @translate
+                        'info' => 'The height of the container in percentage (e.g., 100%) or in pixels (e.g., 600).', // @translate
                     ],
                     'attributes' => [
-                    'id' => 'adno-height',
-                    'size' => 10,
-                    'maxlength' => 10,
-                    'pattern' => '[0-9]+%?'
+                        'id' => 'adno-height',
+                        'size' => 10,
+                        'maxlength' => 10,
+                        'pattern' => '[0-9]+%?'
                     ],
             ]);
     }
